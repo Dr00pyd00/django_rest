057c064e1ae6b933c1f9a87e27fc67a1cdc141ce
Dr00pyd00 <kottakcode@gmail.com>
2026-08-22 12:10:51 +0400

    feat(library) models loan

--- library/models.py
+++ library/models.py
@@ -1,6 +1,8 @@
 
 from django.db import models
 
+from django.conf import settings 
+
 # Create your models here.
 # id est automatique 
 # null=True : autorise NULL en base de donnees  (SQL)
@@ -18,8 +20,22 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     year = models.IntegerField(null=True, blank=True)
 
+    @property
+    def is_available(self):
+        return self.loan_set.filter(return_date__isnull=True).exists()
+
     def __str__(self):
         return self.title
+
+
+class Loan(models.Model):   # emprunt
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    loan_date = models.DateField(auto_now_add=True)    # rempli automaitquement la date actuelle a la creation de l'objet
+    return_date = models.DateField(null=True,blank=True)     # si null : livre NON dispo / si notnull : livre dispo
+
+    def __str__(self):
+        return f'loan {self.book} by {self.user}'
 
 
 
