12572832a82a702e9fbf387fbde576d330e5ce69
Dr00pyd00 <kottakcode@gmail.com>
2026-08-23 13:15:59 +0400

    feat(ssettings) add pagination

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,14 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+# on ajoute la pagination tres global
+REST_FRAMEWORK = {
+        'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 20,
+        }
+
+
+
+
+
